5dc780af63bf70b6f07612853d6b2d81dacf1157
Felix Arbeithuber <felix.arbeithuber@gmail.com>
2025-04-24 20:26:28 +0200

    init

---
+++ src/lru.rs
@@ -0,0 +1,140 @@
+use std::{array, collections::VecDeque};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum CacheHit {
+    Hit,
+    Miss,
+}
+
+pub struct MainMemory<const SIZE: usize, const LINE_SIZE: usize> {
+    data: [u8; SIZE],
+}
+
+impl<const SIZE: usize, const LINE_SIZE: usize> MainMemory<SIZE, LINE_SIZE> {
+    pub fn new(data: [u8; SIZE]) -> Self {
+        const {
+            assert!(
+                is_power_of_two(SIZE),
+                "SIZE of MainMemory is not a power of two"
+            );
+            assert!(
+                is_power_of_two(LINE_SIZE),
+                "LINE_SIZE of MainMemory is not a power of two"
+            );
+        }
+
+        Self { data }
+    }
+
+    pub fn create_cache<const SETS: usize, const LINES: usize>(
+        &self,
+    ) -> LruCache<SIZE, SETS, LINES, LINE_SIZE> {
+        const {
+            assert!(
+                is_power_of_two(SETS),
+                "SIZE of MainMemory is not a power of two"
+            );
+            assert!(
+                is_power_of_two(LINES),
+                "SIZE of MainMemory is not a power of two"
+            );
+            assert!(
+                LINE_SIZE * LINES * SETS <= SIZE,
+                "the LruCache can't be bigger than the MainMemory"
+            )
+        }
+
+        LruCache::new(self)
+    }
+
+    fn get(&self, address: usize) -> [u8; LINE_SIZE] {
+        let addr = address / LINE_SIZE * LINE_SIZE;
+        array::from_fn(|i| self.data.get(addr + i).copied().unwrap_or(0))
+    }
+}
+
+pub struct LruCache<
+    'mm,
+    const SIZE: usize,
+    const SETS: usize,
+    const LINES: usize,
+    const LINE_SIZE: usize,
+> {
+    main_memory: &'mm MainMemory<SIZE, LINE_SIZE>,
+    sets: [CacheSet<LINES, LINE_SIZE>; SETS],
+}
+
+impl<'mm, const SIZE: usize, const SETS: usize, const LINES: usize, const LINE_SIZE: usize>
+    LruCache<'mm, SIZE, SETS, LINES, LINE_SIZE>
+{
+    fn new(main_memory: &'mm MainMemory<SIZE, LINE_SIZE>) -> Self {
+        Self {
+            main_memory,
+            sets: array::from_fn(|_| CacheSet::new()),
+        }
+    }
+
+    pub fn get(&mut self, address: usize) -> ([u8; LINE_SIZE], CacheHit) {
+        let set_addr = address / LINE_SIZE * LINE_SIZE;
+        let set = &mut self.sets[address / LINE_SIZE % LINES];
+
+        let cache_line = set
+            .lines
+            .iter()
+            .enumerate()
+            .find(|(_, line)| line.as_ref().is_some_and(|l| l.set_address == set_addr));
+
+        if let Some((idx, cache_line)) = cache_line {
+            if let Some(cache_line) = cache_line {
+                set.meta
+                    .remove(*set.meta.iter().find(|&&i| i == idx).unwrap());
+                set.meta.push_front(idx);
+                return (cache_line.line.clone(), CacheHit::Hit);
+            }
+        }
+
+        let line = self.main_memory.get(address);
+
+        let lru = set.meta.pop_back().unwrap();
+        set.meta.push_front(lru);
+        set.lines[lru] = Some(CacheLine::new(set_addr, line.clone()));
+
+        return (line, CacheHit::Miss);
+    }
+}
+
+#[derive(Debug, Clone)]
+struct CacheSet<const LINES: usize, const LINE_SIZE: usize> {
+    lines: [Option<CacheLine<LINE_SIZE>>; LINES],
+    meta: VecDeque<usize>,
+}
+
+impl<const LINES: usize, const LINE_SIZE: usize> CacheSet<LINES, LINE_SIZE> {
+    fn new() -> Self {
+        let mut meta = VecDeque::with_capacity(LINES);
+        for i in 0..LINES {
+            meta.push_back(i);
+        }
+
+        Self {
+            lines: array::from_fn(|_| None),
+            meta,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct CacheLine<const LINE_SIZE: usize> {
+    set_address: usize,
+    line: [u8; LINE_SIZE],
+}
+
+impl<const LINE_SIZE: usize> CacheLine<LINE_SIZE> {
+    fn new(set_address: usize, line: [u8; LINE_SIZE]) -> Self {
+        Self { set_address, line }
+    }
+}
+
+const fn is_power_of_two(i: usize) -> bool {
+    i > 1 && i.count_ones() == 1
+}
---
+++ src/main.rs
@@ -0,0 +1,84 @@
+use std::env::current_dir;
+use std::io::{Write, stdout};
+
+use lru::{CacheHit, MainMemory};
+
+mod lru;
+
+fn main() {
+    let main_memory = MainMemory::<128, 4>::new(std::array::from_fn(|i| i as u8));
+    let mut lru_cache = main_memory.create_cache::<8, 4>();
+
+    let mut stdout = stdout();
+    let Some(ref filename) = std::env::args().skip(1).next() else {
+        println!("no file given");
+        return;
+    };
+
+    let Ok(file_data) = std::fs::read_to_string(current_dir().unwrap().join(filename)) else {
+        println!("unable to read file");
+        return;
+    };
+
+    let mut cache_hits_cnt = 0;
+    let mut cache_misses_cnt = 0;
+    for addr_str in file_data.split("\n") {
+        let addr_str = addr_str.trim();
+        if addr_str.is_empty() {
+            continue;
+        }
+
+        let (addr_str, count_str) = addr_str.split_once(":").unwrap_or_else(|| (addr_str, "1"));
+
+        let Ok(address) = usize::from_str_radix(&addr_str[2..], 16) else {
+            stdout
+                .write_fmt(format_args!("Invalid address: {addr_str}"))
+                .unwrap();
+            return;
+        };
+
+        let Ok(count) = usize::from_str_radix(count_str, 10) else {
+            stdout
+                .write_fmt(format_args!("Invalid address: {addr_str}"))
+                .unwrap();
+            return;
+        };
+
+        for i in 0..count {
+            let address = address + i;
+
+            let (data, cache_hit) = lru_cache.get(address);
+            stdout
+                .write_fmt(format_args!(
+                    "{:?}{} ",
+                    data[address % 4],
+                    if cache_hit == CacheHit::Hit { "" } else { "!" }
+                ))
+                .unwrap();
+
+            if cache_hit == CacheHit::Hit {
+                cache_hits_cnt += 1
+            } else {
+                cache_misses_cnt += 1
+            }
+        }
+    }
+
+    stdout
+        .write_fmt(format_args!(
+            "\nHits: {cache_hits_cnt}, Misses: {cache_misses_cnt}\n"
+        ))
+        .unwrap();
+    stdout
+        .write_fmt(format_args!(
+            "Percent Hits: {}\n",
+            f64::from(cache_hits_cnt) / (f64::from(cache_hits_cnt) + f64::from(cache_misses_cnt))
+        ))
+        .unwrap();
+    stdout
+        .write_fmt(format_args!(
+            "Percent Misses: {}\n",
+            f64::from(cache_misses_cnt) / (f64::from(cache_hits_cnt) + f64::from(cache_misses_cnt))
+        ))
+        .unwrap();
+}
